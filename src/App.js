ae10e60084f24fe54a846ebba4be09dbad5b0e28
test <anurag.csharp@gmail.com>
2022-07-10 17:27:06 +0530

    Added New Stuff

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackStats from './components/FeedbackStats'
-import FeedbackForm from './components/FeedbackForm'
-import AboutIconLink from './components/AboutIconLink'
-import AboutPage from './pages/AboutPage'
-import { FeedbackProvider } from './context/FeedbackContext'
+import React from "react"
 
-function App() {
-  return (
-    <FeedbackProvider>
-      <Router>
-        <Header />
-        <div className='container'>
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <>
-                  <FeedbackForm />
-                  <FeedbackStats />
-                  <FeedbackList />
-                </>
-              }
-            ></Route>
 
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
 
-          <AboutIconLink />
-        </div>
-      </Router>
-    </FeedbackProvider>
-  )
+function App()
+{
+    return React.createElement(
+        'div', {className:'Container'},
+         React.createElement('h1', {}, 'My App'))
+    
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import Reactfrom  from 'react'
+import ReactDOM  from 'react-dom'
 import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.render( 
+
+<React.StrictMode> 
+
+ <App /> 
+    
+</React.StrictMode>
+
+
+
+,document.getElementById("root"))
